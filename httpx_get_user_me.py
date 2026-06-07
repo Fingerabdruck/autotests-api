11a2d4f7d759d3d48359329aa4dc69ec4bfa4b05
Fingerabdruck <markuznecov@gmail.com>
2026-06-07 20:09:44 +0300

    Практикуемся в работе с HTTPX

--- httpx_get_user_me.py
+++ httpx_get_user_me.py
@@ -7,12 +7,11 @@ login_payload = {
 
 login_response = httpx.post("http://127.0.0.1:8000/api/v1/authentication/login", json=login_payload)
 login_response_data = login_response.json()
-token_response = login_response_data['token']['accessToken']
 print("Status code: ", login_response.status_code)
 print("Response server: ", login_response_data)
 
 
-headers = {"Authorization": f"Bearer {token_response}"}
+headers = {"Authorization": f"Bearer {login_response_data['token']['accessToken']}"}
 get_user_response = httpx.get("http://127.0.0.1:8000/api/v1/users/me", headers=headers)
 
 get_user_response_data = get_user_response.json()
---
+++ httpx_update_user.py
@@ -0,0 +1,38 @@
+import httpx
+from tools.fakers import get_random_email
+
+create_user_payload = {
+  "email": get_random_email(),
+  "password": "string",
+  "lastName": "string",
+  "firstName": "string",
+  "middleName": "string"
+}
+
+create_user_response = httpx.post("http://127.0.0.1:8000/api/v1/users", json=create_user_payload)
+create_user_response_data = create_user_response.json()
+print("Create user data: ", create_user_response_data)
+
+
+login_payload = {
+    "email" : create_user_payload["email"],
+    "password" : create_user_payload["password"]
+}
+login_response = httpx.post("http://127.0.0.1:8000/api/v1/authentication/login", json=login_payload)
+login_response_data = login_response.json()
+print("Login data: ", login_response_data)
+
+patch_payload = {
+  "email": get_random_email(),
+  "lastName": "string3",
+  "firstName": "string4",
+  "middleName": "string5"
+}
+patch_headers = {"Authorization": f"Bearer {login_response_data['token']['accessToken']}"}
+patch_response = httpx.patch(f"http://127.0.0.1:8000/api/v1/users/{create_user_response_data["user"]["id"]}", json=patch_payload, headers=patch_headers)
+patch_response_data = patch_response.json()
+print("Patch data: ", patch_response_data)
+if patch_response.status_code == 200:
+  print("Patch successful")
+else:
+  print("Patch failed")
